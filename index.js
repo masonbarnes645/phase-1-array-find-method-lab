1d2b0281a9ac0e367383f193a2fef4f4d4e1b0fa
Mason Barnes <barnesmason980@gmail.com>
2024-06-07 11:03:14 -0700

    yur

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(data){
+  const match = data.find(record => record['result'] === 'W')
+  return match ? match['year'] : match
+
+     // Object.prototype.values
+      
+
+
+}
+
+
+
+ 
+
+// superbowlwin is taking object as arguement, and giving me the object who has a key matching w. i need to take that object and return the value of the key named year.
+//when i run it all i get it the whole object
